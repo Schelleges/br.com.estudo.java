bff23be5c0f01d3b4ca26b5474f7a193571bfaec
Ricardo Schelleges <ricardoschelleges@htomail.com>
2020-01-16 02:11:54 -0300

    Create repository and service to category

---
+++ src/main/java/com/ricardoschelleges/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.ricardoschelleges.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ricardoschelleges.cursomc.domain.Categoria;
+import com.ricardoschelleges.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+		public Categoria buscar(Integer id) {
+			Optional<Categoria> obj = repo.findById(id);
+			return obj.orElse(null);
+		}
+}
